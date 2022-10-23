af2ddb034d3118f687a9772ec5bd99382366f17f
PavanSriVenkateshKusampudi <pavansrivenkatesh123@gmail.com>
2022-10-24 00:47:57 +0530

    fourth commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgetComponent } from './forget/forget.component';
+import{ReactiveFormsModule,FormsModule} from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{ MatSidenavModule} from '@angular/material/sidenav';
+import{ MatToolbarModule} from '@angular/material/toolbar';
+import{ MatMenuModule} from '@angular/material/menu';
+import{ MatIconModule} from '@angular/material/icon';
+import{ MatDividerModule} from '@angular/material/divider';
+import{ MatListModule} from '@angular/material/list';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { FormDirective } from './form.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    DashboardComponent,
+    ForgetComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
+    DetailComponent,
+    ContactComponent,
+    AboutComponent,
+    FormDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
