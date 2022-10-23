edd42f9972b39d513559423881b9c317732d8e80
PavanSriVenkateshKusampudi <pavansrivenkatesh123@gmail.com>
2022-10-24 00:44:25 +0530

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { DetailComponent } from './detail/detail.component';
-import { ForgetComponent } from './forget/forget.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [
-  {path:'',redirectTo:'signin',pathMatch:'full'},
-  {path:'signin',component:SigninComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'forget',component:ForgetComponent},
-  {path:'home',component:HomeComponent},
-  {path:'header',component:HeaderComponent},
-  {path:'sidenav',component:SidenavComponent},
-  {path:'detail',component:DetailComponent},
-  {path:'contact',component:ContactComponent},
-  {path:'about',component:AboutComponent}
-
-  
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-project'`, () => {
+  it(`should have as title 'Angular_Project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-project');
+    expect(app.title).toEqual('Angular_Project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_Project app is running!');
   });
 });
